4fd76bb571362647fd40cd756ca8972696098764
Joiyad <20bee012@nith.ac.in>
2022-03-03 13:49:06 +0530

    new

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import "./Homepage.scss";
+import {Homepage2} from './Homepage2';
 
 export default function Homepage() {
   return (
+    <div className="main">
     <div className="homepage">
+      <img className="m-auto" src="/icons/ojas.png" alt="" width="200px"/>
       <h1 className="text-white text-5xl text-center font-serif my-12">
         Team OJAS
       </h1>
-      <p className="text-gray-200 text-2xl text-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        corporis facere reprehenderit tempore sunt ut doloribus, ex sed aut
-        quidem esse! Temporibus ullam nisi inventore ducimus, cum obcaecati?
-        Assumenda necessitatibus eaque reiciendis quis error adipisci omnis
-        minima hic dolore id.
+      <p className="text-gray-400 text-3xl text-center">
+        Departmental Team of Electrical Engineering
       </p>
+    </div>
+    <div className="about max-w-6xl m-auto  ">
+      <h2 className="text-center text-white text-4xl mb-10 ">About us</h2>
+      <div className="about2">
+        <p className=" text-2xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis voluptas id perferendis recusandae? Sequi numquam quidem illum! Delectus quasi ipsam dolores ipsum vel cupiditate voluptatem veritatis, fuga, repellendus voluptatum reiciendis!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis voluptas id perferendis recusandae? Sequi numquam quidem illum! Delectus quasi ipsam dolores ipsum vel cupiditate voluptatem veritatis, fuga, repellendus voluptatum reiciendis!
+        </p>
+        <img src="/images/img1.svg" width="250"/>
+        </div>
+    </div>
+    <Homepage2/>
     </div>
   );
 }
---
+++ src/pages/Homepage/Homepage2.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import "./Homepage2.scss";
+
+export const Homepage2 = () => {
+  return (
+    <div>
+        <div className="homepage2">
+            <h2 className='text-center text-3xl'>Our Projects</h2>
+            <div className="projects1 p-3">
+                <div className="project border-2">
+                    <img src='/images/img1.svg' width="150"/>
+                    <p className="text-center text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sapiente, laborum vel unde recusandae sequi aperiam aspernatur dignissimos facere sit</p>
+                </div>
+                <div className="project border-2">
+                    <img src='/images/img1.svg' width="150"/>
+                    <p className="text-center text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sapiente, laborum vel unde recusandae sequi aperiam aspernatur dignissimos facere sit</p>
+                </div>
+                <div className="project border-2">
+                    <img src='/images/img1.svg' width="150"/>
+                    <p className="text-center text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sapiente, laborum vel unde recusandae sequi aperiam aspernatur dignissimos facere sit</p>
+                </div>
+                <div className="project border-2">
+                    <img src='/images/img1.svg' width="150"/>
+                    <p className="text-center text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sapiente, laborum vel unde recusandae sequi aperiam aspernatur dignissimos facere sit</p>
+                </div>
+            </div>
+        </div>
+        <div className="homepage2">
+            <h2 className='text-center text-3xl'>Our Events</h2>
+            <div className="projects1 p-3 ">
+                <div className="project border-2">
+                    <img src='/images/img1.svg' width="150"/>
+                    <p className="text-center text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sapiente, laborum vel unde recusandae sequi aperiam aspernatur dignissimos facere sit</p>
+                </div>
+                <div className="project border-2">
+                    <img src='/images/img1.svg' width="150"/>
+                    <p className="text-center text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sapiente, laborum vel unde recusandae sequi aperiam aspernatur dignissimos facere sit</p>
+                </div>
+                <div className="project border-2">
+                    <img src='/images/img1.svg' width="150"/>
+                    <p className="text-center text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sapiente, laborum vel unde recusandae sequi aperiam aspernatur dignissimos facere sit</p>
+                </div>
+                <div className="project border-2">
+                    <img src='/images/img1.svg' width="150"/>
+                    <p className="text-center text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sapiente, laborum vel unde recusandae sequi aperiam aspernatur dignissimos facere sit</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
