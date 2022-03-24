f90c1dedc4372be69b3a183391e19c4ac8053374
Joiyad <20bee012@nith.ac.in>
2022-03-24 22:39:41 +0530

    changes

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -4,7 +4,7 @@ import "./Homepage.scss";
 export default function Homepage() {
   return (
     <div className="homepage">
-      <h1 className=" font-serif text-5xl text-center mb-4 md:mb-2 mt-0 md:mt-2">
+      <h1 className=" font-serif text-5xl text-center mb-8 mt-0 md:mt-2">
         Team OJAS
       </h1>
       <p className="desc text-2xl text-center md:text-cyan-800">
