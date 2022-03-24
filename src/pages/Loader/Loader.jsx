7c4d7ccf2b9f6142215feb98243092064e2eb2e8
Joiyad <20bee012@nith.ac.in>
2022-03-24 15:05:49 +0530

    updated

--- src/pages/Loader/Loader.jsx
+++ src/pages/Loader/Loader.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './loader.scss';
-import 'animate.css';
 
 import PulseLoader from "react-spinners/PulseLoader";
 
 export default function loader () {
   return (
     <div>
-        <div className=" load animate__animated animate__zoomIn animate__slower animate__repeat-1	">
+        <div className=" load">
             
             {/* <img src="/icons/ojas.png" alt="" width="80px"/> */}
             <PulseLoader color={'#DDE9E7'} size={15} margin={10}></PulseLoader>
