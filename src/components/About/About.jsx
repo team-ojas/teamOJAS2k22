9325d133199d21bd2ec518834b0be5790765a0de
Joiyad <20bee012@nith.ac.in>
2022-04-06 17:43:24 +0530

    changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
           <div className='p-0 md:p-8'><p className="textt text-center md:text-left">Team OJAS , the departmental team of Electrical Engineering at National Institute of Technology, Hamirpur is a society driven by the motto “Fuelled by innovation, driven by motivation” that strives to innovate viable and smart solutions for the many problems of our lives. We conduct various technical as well as non-technical events throughout the year aimed at accelerating students interest towards electrical engineering and nurturing a learning yet enjoyable atmosphere where everyone can exhibit their ideas and get a chance to turn their dreams into reality. We have also won the prize in “Best Event Execution” category in NIMBUS 2021.
           </p>
           </div>
-          <video className='rounded-xl' width="280" height="280" autoPlay loop>
+          <video className='rounded-xl' width="350" height="350" autoPlay loop>
             <source src='/images/ojaslogo.mp4' type='video/mp4' />
           </video>
         </div>
