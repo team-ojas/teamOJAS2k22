696213f72595ab3a0f9d2812bfcfbdf22f83ebab
Joiyad <20bee012@nith.ac.in>
2023-01-15 16:11:33 +0530

    import error resolved

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import "./Homepage.scss";
 import Background from "../../components/Background/Background";
 import { Footer } from "../../components";
 import CC from '../../components/OurTeam/CC';
 import Events from '../../components/Events/Events';
 import About from "../../components/About/About";
 import Background2 from "../../components/Background/Background2";
-
-
 
 export default function Homepage() {
   return (
